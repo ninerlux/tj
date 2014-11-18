f49c9a83e3a8ecef5bbaa3a5eae7757cb0aa9734
Xin Lu <xinlu@cs.columbia.edu>
2014-11-18 15:26:38 -0500

    some functions added

--- TrackJoin2.cpp
+++ TrackJoin2.cpp
@@ -33,9 +33,43 @@ static void *scan_and_send(void *param) {
     Record *r = NULL;
     // Send each record in T to destination node
     for (int i = 0; i < T->num_records; i++) {
-        if (h.find_s(T->records[i].k, &r, h.getNum(), 10) < 0) {
+        // check if key is duplicate
+        if (h.find(T->records[i].k, &r, h.getNum(), 10) < 0) {
+            h.add(&(T->records[i]));
             dest = h.hash32(T->records[i].k) % hosts;
+            if (dbs[dest].size + sizeof(Record) > BLOCK_SIZE) {
+                CL->send_end(dbs[dest], dest, 0); //msgs with tag 0 are sent to process T
+                //printf("Scan - Node %d send data block to node %d with %lu records\n", local_host, dest, dbs[dest].size / sizeof(Record));
+                //fflush(stdout);
+                while (!CL->send_begin(&dbs[dest], dest, 0));
+                dbs[dest].size = 0;
+            }
+            *((Record *) dbs[dest].data + dbs[dest].size / sizeof(Record)) = T->records[i];
+            dbs[dest].size += sizeof(Record);
+            assert(dbs[dest].size <= BLOCK_SIZE);
         }
-
     }
+
+    // Send last partially filled data blocks and end flags to all nodes
+    for (dest = 0; dest < hosts; dest++) {
+        // Send last data blocks
+        if (dbs[dest].size > 0) {
+            assert(dbs[dest].size <= BLOCK_SIZE);
+            CL->send_end(dbs[dest], dest, 1);
+            //printf("Scan - Node %d send data block to node %d with %lu records\n", local_host, dest, dbs[dest].size / sizeof(Record));
+            //fflush(stdout);
+        }
+        // Send end flags
+        while (!CL->send_begin(&dbs[dest], dest, 1));
+        dbs[dest].size = 0;
+        CL->send_end(dbs[dest], dest, 1);
+        //printf("Scan - Node %d send end flag node %d with size %lu\n", local_host, dest, dbs[dest].size);
+        //fflush(stdout);
+    }
+
+    return NULL;
+}
+
+static void *recv_and_build(void *param) {
+
 }
